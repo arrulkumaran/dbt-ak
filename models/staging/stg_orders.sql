d21ca69aae98c2c2d8a1b9ba470c765d005f70c9
arrulkumaran@gmail.com <arrulkumaran@gmail.com>
2025-07-26 08:26:22 +0000

    developing staging model

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,14 @@
+select 
+-- from raw orders
+a.orderid,
+a.orderdate,
+a.shipdate,
+a.shipmode,
+a.ordersellingprice - a.ordercostprice as orderprofit,
+--from raw customers
+c.customername,
+c.segment,
+c.country
+from {{ ref('raw_orders') }} as a 
+left join {{ ref('raw_customers') }} as c 
+on a.CUSTOMERID = c.CUSTOMERID
